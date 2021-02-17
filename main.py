4ff27f80d393938aadffd528896af6906d61d605
drammma <Safonova14092004@yandex.ru>
2021-02-17 12:05:03 +0400

    добавление новых файлов

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+
+import sys
+import requests
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPixmap
+from DesignUI import Ui_MainWindow
+
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('Большая задача по Maps API.')
+
+        self.pushButton.clicked.connect(self.generation_map)
+
+    def generation_map(self):
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.line_coordinateX.text()},{self.line_coordinateY.text()}&z={self.sbox_coordinateZ.text()}&l=map"
+        response = requests.get(map_request)
+
+        self.map_file = "map.png"
+
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+        self.display_map()
+
+    def display_map(self):
+        self.pixmap = QPixmap(self.map_file)
+        self.picture.setPixmap(self.pixmap)
+        os.remove(self.map_file)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    application = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.excepthook = except_hook
+    sys.exit(application.exec())
